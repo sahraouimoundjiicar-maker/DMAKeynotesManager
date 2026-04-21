8dcdbdf980d0d20f0cab395053bd5f54e099f863
sahraouimoundjiicar-maker <sahraoui.moundji.icar@gmail.com>
2026-04-21 02:41:49 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Connexion from './pages/Connexion';
-import Projets from './pages/Projets';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import Connexion    from './pages/Connexion';
+import Projets      from './pages/Projets';
 import Utilisateurs from './pages/Utilisateurs';
-import Keynotes from './pages/Keynotes';
+import Keynotes     from './pages/Keynotes';
+import Header       from './components/Header';
 
+// Pages qui affichent le header
+const PAGES_AVEC_HEADER = ['/projets', '/utilisateurs', '/keynotes'];
+
+function Layout() {
+  const location        = useLocation();
+  const afficherHeader  = PAGES_AVEC_HEADER.includes(location.pathname);
+
+  return (
+    <>
+      {afficherHeader && <Header />}
+      <Routes>
+        <Route path="/"             element={<Connexion />} />
+        <Route path="/connexion"    element={<Connexion />} />
+        <Route path="/projets"      element={<Projets />} />
+        <Route path="/utilisateurs" element={<Utilisateurs />} />
+        <Route path="/keynotes"     element={<Keynotes />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Connexion />} />
-        <Route path="/connexion" element={<Connexion />} />
-        <Route path="/projets" element={<Projets />} />
-        <Route path="/utilisateurs" element={<Utilisateurs />} />
-        <Route path="/keynotes" element={<Keynotes />} />
-      </Routes>
+      <Layout />
     </BrowserRouter>
   );
 }
